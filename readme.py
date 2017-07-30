ab229b212558b501d31c4de524684d1030f9b0cd
lk_home <xcsliuke@pku.edu.cn>
2017-07-30 18:11:38 +0800

    'room'

--- readme.py
+++ readme.py
@@ -5,3 +5,11 @@ print ('i just want to say hi to my git')
 # 现在加一些新内容，看看效果
 # 现在来测试关于对修改的管理
 # 第二次修改
+class room():
+    def __init__(self):
+        self.area = 25
+        self.charge = 2400
+
+    def cal(self):
+        unit_price = self.charge / self.area
+        return unit_price
